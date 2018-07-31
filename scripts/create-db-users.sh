bde25aa785d18842d8207d184e1400a793a97f4b
Slava Snezhkov <vsnezhkov@gmail.com>
2018-07-31 14:18:54 +0300

    improve configuration

---
+++ scripts/create-db-users.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+createuser user_main
+createuser user_rhea
+createuser user_loge
+createuser user_ymir
--- scripts/recreate-test-db.sh
+++ scripts/recreate-test-db.sh
@@ -16,7 +16,7 @@ createdb bloqly_ymir
 
 createdb bloqly_test
 
-psql -d bloqly_main -c "alter user "$USER" with password 'password';"
-psql -d bloqly_rhea -c "alter user "$USER" with password 'password';"
-psql -d bloqly_loge -c "alter user "$USER" with password 'password';"
-psql -d bloqly_ymir -c "alter user "$USER" with password 'password';"
+psql -d bloqly_main -c "alter user user_main with password 'password_main';"
+psql -d bloqly_rhea -c "alter user user_rhea with password 'password_rhea';"
+psql -d bloqly_loge -c "alter user user_loge with password 'password_loge';"
+psql -d bloqly_ymir -c "alter user user_ymir with password 'password_ymir';"
